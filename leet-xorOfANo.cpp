56eac4f09a85c6d820cca4807200006a345ddbd4
Kum29 <142295335+Kum29@users.noreply.github.com>
2024-05-27 00:23:18 +0530

    Add files via upload
    3158. Find the XOR of Numbers Which Appear Twice

---
+++ leet-xorOfANo.cpp
@@ -0,0 +1,13 @@
+
+    int duplicateNumbersXOR(vector<int>& nums) {
+     vector<int>x;
+    sort(nums.begin(), nums.end());
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]) x.push_back(nums[i]);
+        }
+        int r=0;
+        for(int i=0;i<x.size();i++){
+            r^=x[i];
+        }
+        return r;
+    }
